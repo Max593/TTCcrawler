0f9675b5ceb7870cc97b89190d706d7e14fe58b4
raresboza <raresboza@gmail.com>
2021-03-10 17:28:29 +0100

    major refactoring to crawler.py to allow for gui

--- crawler.py
+++ crawler.py
@@ -1,14 +1,19 @@
 import time
 import datetime
 import threading
-import simpleaudio
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from conversions import *
 from alarm import *
+from os import system
 
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from IPython.display import display
 # lxml required for selenium
@@ -17,7 +22,12 @@ from IPython.display import display
 
 class Crawler:
 
+    # Initiating null variables
     searchUrl = None
+    driver = None
+    running = False
+    urlArray = []
+    previousFrames = []
     chrome_options = Options()
 
     testingUrl = 'https://eu.tamrieltradecentre.com/pc/Trade/SearchResult?SearchType=Sell&ItemID=&ItemNamePattern=Mother%27s+Sorrow&ItemCategory1ID=1&ItemCategory2ID=2&ItemCategory3ID=18&ItemTraitID=13&ItemQualityID=&IsChampionPoint=true&IsChampionPoint=false&LevelMin=160&LevelMax=&MasterWritVoucherMin=&MasterWritVoucherMax=&AmountMin=&AmountMax=&PriceMin=&PriceMax=20000'
@@ -25,72 +35,121 @@ class Crawler:
 
     def __init__(self, url):
         self.searchUrl = url
+
         # Local chrome folder
         self.chrome_options.binary_location = 'ChromeStandalone/App/Chrome-bin/chrome.exe'
-        self.chrome_options.add_argument('--headless')
-        self.chrome_options.add_argument('--no-sandbox')
-        self.chrome_options.add_argument('--disable-dev-shm-usage')
 
-    def request_item(self):
-        previousFrames = None
+        # Setting up a Chrome session - comment for debug
+        #self.chrome_options.add_argument('--headless')
+        #self.chrome_options.add_argument('--no-sandbox')
+        #self.chrome_options.add_argument('--disable-dev-shm-usage')
 
-        while 1:
-            # Create a new Chrome session
-            driver = webdriver.Chrome('chromedriver.exe', options=self.chrome_options)
-            # Load the web page
-            driver.get(self.searchUrl)
-            # Wait for the page to fully load
-            # driver.implicitly_wait(10)
+        # Create a new Chrome session
+        self.driver = webdriver.Chrome('chromedriver.exe', options=self.chrome_options)
+
+    def open_pages(self):
+        """Method that opens all the urls in the chrome browser"""
+        self.driver.get(self.urlArray[0])
+        for i in range(1, len(self.urlArray)):
+            self.driver.execute_script("window.open();")
+            self.driver.switch_to.window(self.driver.window_handles[i])
+            self.driver.get(self.urlArray[i])
+        self.driver.switch_to.window(self.driver.window_handles[0])
+
+    def refresh_pages(self):
+        """Method that refreshes all tabs"""
+        self.driver.refresh()
+        for i in range(1, len(self.urlArray)):
+            self.driver.switch_to.window(self.driver.window_handles[i])
+            self.driver.refresh()
+        self.driver.switch_to.window(self.driver.window_handles[0])
+
+    def add_url(self, url: str):
+        """Method that adds a url to the url array"""
+        self.urlArray.append(url)
+
+    def request_items_from_urls(self):
+        """Method that requests items from all tabs"""
+        self.running = True
+        while self.running is True:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_all_elements_located((By.ID, 'search-result-view')))
+            content, found = self.request_item(0)
+            print(content[0])
+            # here we should send it over to the gui
+            for i in range(1, len(self.urlArray)):
+                self.driver.switch_to.window(self.driver.window_handles[i])
+                WebDriverWait(self.driver, 10).until(EC.visibility_of_all_elements_located((By.ID, 'search-result-view')))
+                content, found = self.request_item(i)
+                print(content[0])
+                # here we should send it over to the gui
+            self.driver.switch_to.window(self.driver.window_handles[0])
             time.sleep(10)
+            self.refresh_pages()
 
-            soup = BeautifulSoup(driver.page_source, 'lxml')
-            tables = soup.find_all('table')
-            dfs = pd.read_html(str(tables))
 
-            # pretty print settings
-            pd.set_option('display.max_rows', None)
-            pd.set_option('display.max_columns', None)
-            pd.set_option('display.width', 5000)
-            pd.set_option('display.colheader_justify', 'center')
-            pd.set_option('display.precision', 2)
+    def request_item(self, position:int):
 
-            pageFrames = dfs[0]
-            pageFrames = pageFrames[pageFrames['Item'].notna()]
-            pageFrames = pageFrames.reset_index(drop=True) # Creates a unified index
-            pageFrames = pageFrames.iloc[1:] # Deletes column names
-            # Final Price column with int values
-            prices = []
-            for price in pageFrames["Price"]:
-                prices.append(price_to_int(price))
-            pageFrames["Final Price"] = prices
-            # Time
-            last_seen_minutes = []
-            for last_seen in pageFrames["Last Seen"]:
-                last_seen_minutes.append(time_to_int(last_seen))
-            pageFrames["Last Seen Minutes"] = last_seen_minutes
+        # while 1:
+        # driver.execute_script("window.open();")
+        # driver.switch_to_window(driver.window_handles[0])
+        # Load the web page
+        # self.driver.get(self.searchUrl)
+        # Wait for the page to fully load
+        # driver.implicitly_wait(10)
+        # time.sleep(10)
+        content = []
+        found = False
+        soup = BeautifulSoup(self.driver.page_source, 'lxml')
+        tables = soup.find_all('table')
+        dfs = pd.read_html(str(tables))
 
-            # Pretty printing
-            print("\n\n*-----------------------------------------------------*")
-            display(pageFrames)
+        # pretty print settings
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', 5000)
+        pd.set_option('display.colheader_justify', 'center')
+        pd.set_option('display.precision', 2)
 
-            if previousFrames is not None:
-                resultFrames = dataframe_difference(pageFrames, previousFrames, which='left_only')
-                print("-------------------------------------------------------")
-                if resultFrames.empty is False:
-                    # display new items
-                    display(resultFrames)
+        pageFrames = dfs[0]
+        pageFrames = pageFrames[pageFrames['Item'].notna()]
+        pageFrames = pageFrames.reset_index(drop=True) # Creates a unified index
+        pageFrames = pageFrames.iloc[1:] # Deletes column names
+        # Final Price column with int values
+        prices = []
+        for price in pageFrames["Price"]:
+            prices.append(price_to_int(price))
+        pageFrames["Final Price"] = prices
+        # Time
+        last_seen_minutes = []
+        for last_seen in pageFrames["Last Seen"]:
+            last_seen_minutes.append(time_to_int(last_seen))
+        pageFrames["Last Seen Minutes"] = last_seen_minutes
 
-                    # notify
-                    sound = threading.Thread(target=sound_alarm())
-                    sound.start()
-                    sound.join()
-                else:
-                    # timestamp to notify the user when was the last time a request took place
-                    print("No new queries found in this search. ", datetime.datetime.now())
+        # Pretty printing
+        content.append(pageFrames)
+        #print("\n\n*-----------------------------------------------------*")
+        #display(pageFrames)
 
-            print("*-----------------------------------------------------*")
-            # update previous dataframe for comparison
-            previousFrames = pageFrames
-            # close browser
-            driver.quit()
-            time.sleep(15)
+        if len(self.previousFrames) == len(self.urlArray):
+            resultFrames = dataframe_difference(pageFrames, self.previousFrames[position], which='left_only')
+            #print("\n-------------------------------------------------------")
+            if resultFrames.empty is False:
+                # display new items
+                content.append(resultFrames)
+                #display(resultFrames)
+
+                # notify
+                found = True
+                sound = threading.Thread(target=sound_alarm())
+                sound.start()
+                sound.join()
+            else:
+                # timestamp to notify the user when was the last time a request took place
+                print("No new queries found in this search. ", datetime.datetime.now())
+        # print("\n*-----------------------------------------------------*\n\n\n")
+        # update previous dataframe for comparison
+        self.previousFrames.insert(position, pageFrames)
+        return content, found
+        # close browser
+        # self.driver.quit()
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from crawler import *
+import time
 if __name__ == '__main__':
     # fast changing
     #url = 'https://eu.tamrieltradecentre.com/pc/Trade/SearchResult?SearchType=Sell&ItemID=&ItemNamePattern=mother%27s+sorrow&ItemCategory1ID=&ItemTraitID=&ItemQualityID=&IsChampionPoint=false&LevelMin=&LevelMax=&MasterWritVoucherMin=&MasterWritVoucherMax=&AmountMin=&AmountMax=&PriceMin=&PriceMax='
@@ -6,4 +7,10 @@ if __name__ == '__main__':
     #url = 'https://eu.tamrieltradecentre.com/pc/Trade/SearchResult?SearchType=Sell&ItemID=6132&ItemNamePattern=Perfect+Roe&ItemCategory1ID=&ItemTraitID=&ItemQualityID=&IsChampionPoint=false&LevelMin=&LevelMax=&MasterWritVoucherMin=&MasterWritVoucherMax=&AmountMin=&AmountMax=&PriceMin=&PriceMax=20000'
     url = input("Paste the TTC url here: ")
     crawler = Crawler(url)
-    crawler.request_item()
+    #crawler.request_item()
+    crawler.add_url('https://eu.tamrieltradecentre.com/pc/Trade/SearchResult?SearchType=Sell&ItemID=&ItemNamePattern=Mother%27s+Sorrow&ItemCategory1ID=1&ItemCategory2ID=2&ItemCategory3ID=18&ItemTraitID=13&ItemQualityID=&IsChampionPoint=true&IsChampionPoint=false&LevelMin=160&LevelMax=&MasterWritVoucherMin=&MasterWritVoucherMax=&AmountMin=&AmountMax=&PriceMin=&PriceMax=20000')
+    crawler.add_url('https://eu.tamrieltradecentre.com/pc/Trade/SearchResult?SearchType=Sell&ItemID=&ItemNamePattern=mother%27s+sorrow&ItemCategory1ID=&ItemTraitID=&ItemQualityID=&IsChampionPoint=false&LevelMin=&LevelMax=&MasterWritVoucherMin=&MasterWritVoucherMax=&AmountMin=&AmountMax=&PriceMin=&PriceMax=')
+    crawler.open_pages()
+    time.sleep(3)
+    crawler.refresh_pages()
+    crawler.request_items_from_urls()
